361341a0804e43704e08184bae48870734eaa1c2
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-27 23:46:46 +0800

    create modal for adding new boards

--- Frontend/src/components/EmptyCard.jsx
+++ Frontend/src/components/EmptyCard.jsx
@@ -1,8 +1,10 @@
 import { FiPlusCircle } from "react-icons/fi";
-
+import { BoardContext } from "../contexts/BoardContext";
+import { useContext } from "react";
 const EmptyCard = () => {
+    const {setOpenNewBoard} = useContext(BoardContext);
   return (
-    <div className="flex justify-center items-center bg-[#050916] p-8 border rounded-lg cursor-pointer min-h-full">
+    <div className="flex justify-center items-center bg-[#050916] p-8 border rounded-lg cursor-pointer min-h-full" onClick={()=>setOpenNewBoard(true)}>
       <FiPlusCircle size={60}/>
     </div>
   );
