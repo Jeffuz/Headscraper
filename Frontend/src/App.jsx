ed124aec681ad59e5768367cd85106d0ed26caa5
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-27 12:19:07 +0800

    finish navbar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,14 @@
-
+import Navbar from "./components/Navbar";
 
 function App() {
-
   return (
-    <>
-      <div className="bg-black">
-        hi
+    <div className="bg-secondary h-screen">
+      {/* Navbar */}
+      <div className="py-8 px-16">
+        <Navbar />
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
