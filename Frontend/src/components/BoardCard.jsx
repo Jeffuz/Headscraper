203f8b3f7861b19f3cc68f4145a7727fd6dad2a6
Jeff Zhang <52511888+Jeffuz@users.noreply.github.com>
2024-07-28 18:00:57 +0800

    delete board modal (no end point )

--- Frontend/src/components/BoardCard.jsx
+++ Frontend/src/components/BoardCard.jsx
@@ -1,13 +1,22 @@
-import { CiViewBoard } from "react-icons/ci";
 import { CiCalendar } from "react-icons/ci";
+import { FaRegTrashAlt } from "react-icons/fa";
+import Modal from "./Modal";
+import { useState } from "react";
+import { TfiBlackboard } from "react-icons/tfi";
+
 
 const BoardCard = ({ title, description, lastUpdate }) => {
+  const [open, setOpen] = useState(false);
   return (
     <div className="flex flex-col bg-[#050916] p-8 border rounded-lg gap-6 cursor-pointer min-h-full">
       {/* Title Bar */}
       <div className="flex justify-between items-center">
         <div className="font-semibold text-2xl">{title}</div>
-        <CiViewBoard size={30} />
+        <FaRegTrashAlt
+          size={25}
+          className="hover:text-red-600 transition delay-50"
+          onClick={()=>setOpen(true)}
+        />
       </div>
       {/* Description */}
       <div>{description}</div>
@@ -16,6 +25,39 @@ const BoardCard = ({ title, description, lastUpdate }) => {
         <CiCalendar size={20} />
         <div className="text-sm"> Last updated on {lastUpdate}</div>
       </div>
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div className="flex flex-col text-white">
+          {/* Company Name */}
+          <div className="text-primary flex items-center gap-3">
+            <TfiBlackboard size={28} />
+            <div className="text-white font-bold text-lg">Headscraper</div>
+          </div>
+          {/* Delete Board */}
+          <div className="flex flex-col py-5 gap-2">
+            <div className="font-bold text-2xl">Deleting the board?</div>
+            <div className="text-md">
+              Click &apos;Delete&apos; to confirm.
+            </div>
+          </div>
+          {/* Logout */}
+          <div className="flex">  
+            <button
+              onClick={() => setOpen(false)}
+              className="w-[50%] flex items-center justify-center gap-2 bg-primary hover:bg-primary/90 text-secondary rounded-lg px-24 py-2 font-semibold"
+            >
+              <div>Cancel</div>
+            </button>
+            <button
+              onClick={() => {
+                setOpen(false);
+              }}
+              className="w-[50%] flex justify-center items-center"
+            >
+              <div className="hover:text-red-500 delay-50 transition">Delete</div>
+            </button>
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 };
